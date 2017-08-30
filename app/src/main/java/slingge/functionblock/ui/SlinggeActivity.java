e5eb214c4e1d77368d0e77869561b5ec236988ee
Slingge <linke13523089752>
2017-08-30 16:35:15 +0800

    滑动finish3

--- app/src/main/java/slingge/functionblock/ui/SlinggeActivity.java
+++ app/src/main/java/slingge/functionblock/ui/SlinggeActivity.java
@@ -1,6 +1,5 @@
 package slingge.functionblock.ui;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
--- app/src/main/java/slingge/functionblock/ui/moveFinish/SwipeBackLayout.java
+++ app/src/main/java/slingge/functionblock/ui/moveFinish/SwipeBackLayout.java
@@ -121,7 +121,7 @@ public class SwipeBackLayout extends FrameLayout {
                 break;
             case MotionEvent.ACTION_UP:
                 isSilding = false;
-                if (mContentView.getScrollX() <= -viewWidth / 2) {
+                if (mContentView.getScrollX() <= -viewWidth / 4) {
                     isFinish = true;
                     scrollRight();
                 } else {
